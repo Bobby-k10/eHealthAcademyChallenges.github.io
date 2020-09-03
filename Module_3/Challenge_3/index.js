6d8fa34d90b9b32348239ffbe84e9b80593f3d4e
Bobby Kuhwa <69642753+Bobby-k10@users.noreply.github.com>
2020-09-03 12:33:46 +0100

    Add files via upload
    eHA Academy Cohort 2 Assignments/Challenges/Projects

---
+++ Module_3/Challenge_3/index.js
@@ -0,0 +1,10 @@
+/*Get access to my button here */
+let button = document.getElementById("change");
+let onclick = document.getElementsByTagName('button');
+
+/* Function code goes here */
+
+function changeMe() {
+    let meColor = ('blue');
+    document.body.style.backgroundColor = meColor;
+}
